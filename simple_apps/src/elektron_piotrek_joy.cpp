8f47bf6e126ccce2f4778f1d2f918eacca86209d
eee <eee@eee2.(none)>
2011-10-28 17:28:21 +0200

    a

--- simple_apps/src/elektron_piotrek_joy.cpp
+++ simple_apps/src/elektron_piotrek_joy.cpp
@@ -13,6 +13,7 @@ private:
 	ros::NodeHandle nh_;
 
 	int linear_, angular_;
+	int turbo_, stop_, feature1_, feature2_;
 	double l_scale_, a_scale_;
 	ros::Publisher vel_pub_;
 	ros::Subscriber joy_sub_;
@@ -21,9 +22,13 @@ private:
 
 ElektronTeleopJoy::ElektronTeleopJoy() {
 	nh_.param("axis_linear", linear_, 1);
-	nh_.param("axis_angular", angular_, 0);
+	nh_.param("axis_angular", angular_, 3);
 	nh_.param("scale_angular", a_scale_, 1.0);
 	nh_.param("scale_linear", l_scale_, 0.23);
+	nh_.param("button_turbo", turbo_, 7);
+	nh_.param("button_stop", stop_, 6);
+	nh_.param("button_feature1", feature1_, 1);
+	nh_.param("button_feature2", feature2_, 2);
 
 	vel_pub_ = nh_.advertise<geometry_msgs::Twist> ("cmd_vel", 1);
 
@@ -36,6 +41,12 @@ void ElektronTeleopJoy::joyCallback(const joy::Joy::ConstPtr& joy) {
 	geometry_msgs::Twist vel;
 	vel.angular.z = a_scale_ * joy->axes[angular_];
 	vel.linear.x = l_scale_ * joy->axes[linear_];
+
+	vel.angular.x = joy->buttons[turbo_];
+	vel.angular.y = joy->buttons[stop_];
+	vel.linear.y = joy->buttons[feature1_];
+	vel.linear.z = joy->buttons[feature2_];
+
 	vel_pub_.publish(vel);
 
 	ROS_DEBUG("joy: %d %d\n", (int)vel.angular.z, (int)vel.linear.x);
